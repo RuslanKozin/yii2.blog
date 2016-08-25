dcf5b71eed8b232d6b6b823ad4951f0754cdb239
RuslanKozin <ruslan3723@mail.ru>
2016-08-25 15:37:38 +0300

    Внес изменения

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -5,8 +5,17 @@ namespace app\controllers;
 
 class PostController extends AppController
 {
-    public function actionIndex()
+    public function actionIndex() //экшн\действие
     {
-        return 'Hello, world!' ;
+        $hello = 'Привет, мир!';
+        $hi = 'Hi!';
+        //return $this->render('index', ['var' => $hello, 'hi' => $hi]); //Один варинт передачи данных в вид
+        return $this->render('index', compact('hello', 'hi'));  //Второй вид передачи данных в вид
     }
+
+    public function actionTest() //экшн\действие
+    {
+        return $this->render('test');
+    }
+
 }
