e1518ecf6eb86b2a422cb31c175a34996746ba2b
RuslanKozin <ruslan3723@mail.ru>
2016-08-25 15:43:40 +0300

    Добавил код для принятия данных от пользователя через адресную строку

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -5,12 +5,12 @@ namespace app\controllers;
 
 class PostController extends AppController
 {
-    public function actionIndex() //экшн\действие
+    public function actionIndex($name = 'Гость') //экшн\действие
     {
         $hello = 'Привет, мир!';
         $hi = 'Hi!';
         //return $this->render('index', ['var' => $hello, 'hi' => $hi]); //Один варинт передачи данных в вид
-        return $this->render('index', compact('hello', 'hi'));  //Второй вид передачи данных в вид
+        return $this->render('index', compact('hello', 'hi', 'name'));  //Второй вид передачи данных в вид
     }
 
     public function actionTest() //экшн\действие
