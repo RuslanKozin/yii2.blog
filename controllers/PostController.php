76da310f08957d61a40103a280fc9f2367506770
RuslanKozin <ruslan3723@mail.ru>
2016-08-26 14:20:22 +0300

    Внес изменения:
    1. перенес действия из actionIndex в actionTest
    2. в actionIndex внес действия для вывода статей из базы данных

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -1,21 +1,25 @@
 <?php
 
 namespace app\controllers;
+use app\models\Post; //Подключаем модель Post
+
 
 
 class PostController extends AppController
 {
-    public function actionIndex($name = 'Гость') //экшн\действие
+    public function actionIndex() //экшн\действие
+    {
+        $posts = Post::find()->all() ;
+            //Post:: - объект модели. find - объект запроса. all - выбрать все
+        return $this->render('index', compact('posts'));
+    }
+
+    public function actionTest($name = 'Гость') //экшн\действие
     {
         $hello = 'Привет, мир!';
         $hi = 'Hi!';
         //return $this->render('index', ['var' => $hello, 'hi' => $hi]); //Один варинт передачи данных в вид
         return $this->render('index', compact('hello', 'hi', 'name'));  //Второй вид передачи данных в вид
-    }
-
-    public function actionTest() //экшн\действие
-    {
-        return $this->render('test');
     }
 
 }
