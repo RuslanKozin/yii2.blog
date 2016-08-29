3ce0258b673cbd5dd375bd8923a451f30cbbe144
RuslanKozin <ruslan3723@mail.ru>
2016-08-29 20:12:57 +0300

    Добавил комментарии и код для вывода информации из базы данных с пагинацией

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace app\controllers;
-use app\models\Post; //Подключаем модель Post
+use app\models\Post;    //Подключаем модель Post
+use yii\data\Pagination;
 
 
 
@@ -9,9 +10,17 @@ class PostController extends AppController
 {
     public function actionIndex() //экшн\действие
     {
-        $posts = Post::find()->select('id, title, excerpt')->all() ;
-            //Post:: - объект модели. find - объект запроса. all - выбрать все. select - выборка
-        return $this->render('index', compact('posts'));
+                    //Вывод информации БЕЗ ПАГИНАЦИИ
+      /*  $posts = Post::find()->select('id, title, excerpt')->all() ;
+             Post:: - объект модели. find - объект запроса. all - выбрать все. select - выборка  */
+
+                    //Вывод информации С ПАГИНАЦИЕЙ
+        $query = Post::find()->select('id, title, excerpt')->orderBy('id DESC');
+        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 4, 'pageSizeParam' => false, 'forcePageParam' => false]);
+            /*pageSize - кол-во записей на стр., pageSizeParam - , forcePageParam - */
+        $posts = $query->offset($pages->offset)->limit($pages->limit)->all();
+            //offset - отступ, all - выбрать все.
+        return $this->render('index', compact('posts', 'pages'));
     }
 
     public function actionTest($name = 'Гость') //экшн\действие
