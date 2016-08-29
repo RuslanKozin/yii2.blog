42819e6d31c0c82df962a29d825b7bbdc54431be
RuslanKozin <ruslan3723@mail.ru>
2016-08-29 19:23:55 +0300

    Добавил код debug

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -6,5 +6,7 @@ use yii\web\Controller; //Основной контроллер
 
 class AppController extends Controller //расширяю основной контроллер
 {
-
+    public function debug($arr){
+        echo '<pre>' . print_r($arr, true) . '</pre>>';
+    }
 }
