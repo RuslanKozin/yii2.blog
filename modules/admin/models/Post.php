21a9bbbf745562bd584eeb6f4c904b3d37c1b1be
RuslanKozin <ruslan3723@mail.ru>
2016-08-30 14:55:24 +0300

    Сгенерировал модель админки с помощью Gii

---
+++ modules/admin/models/Post.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property string $id
+ * @property string $title
+ * @property string $excerpt
+ * @property string $text
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'excerpt', 'text'], 'required'],
+            [['text'], 'string'],
+            [['title', 'excerpt'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'excerpt' => 'Excerpt',
+            'text' => 'Text',
+        ];
+    }
+}
